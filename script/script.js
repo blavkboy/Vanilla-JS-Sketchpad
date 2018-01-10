3390228826cc30aeaa33711ec0bf2a28161e94c1
blavkboy <g.mohlamonyane@gmail.com>
2018-01-10 07:36:12 +0200

    converted from jQuery to vanilla JS

--- script/script.js
+++ script/script.js
@@ -1,116 +1,142 @@
-$(document).ready(function(){
-	var rowCount = $('.row').length;
-	var gridSize = 1;
-	var gridDimension = 500;
-	function resizeDivs(divideBy){
-		var newDimensions = gridDimension / divideBy;
-		$('.row').css({'height':newDimensions});
-		$('.cell').css({'height':newDimensions, 'width':newDimensions});
-	}
+const grid = document.querySelector('#grid');
+const rowCount = function(){
+	return document.querySelectorAll('.row').length;
+}
 
-	var trulyRandomColorPicker = function(){
-		var colorList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'f', 'b', 'c', 'e', 'a', 'd'];
-		var randColor = '';
-		while (randColor.length != 6){
-			randColor += colorList[Math.round(Math.random() * (colorList.length - 1))];
+let byCell = 16;
+
+const trulyRandomColorPicker = function(){
+	var colorList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'f', 'b', 'c', 'e', 'a', 'd'];
+	var randColor = '';
+	while (randColor.length != 6){
+		randColor += colorList[Math.round(Math.random() * (colorList.length - 1))];
+	}
+	return '#' + randColor;
+}
+
+const addRows = function(){
+	let row = document.createElement('div');
+	row.classList.add('row');
+	grid.appendChild(row);
+}
+
+const createCells = function(numOfCells){
+	let functionIterator = 0;
+	while (functionIterator != numOfCells){
+		let newCell = document.createElement('div');
+		newCell.classList.add('cell');
+		newCell.classList.add('column');
+		functionIterator++;
+	}
+}
+
+const makeTheGrid = function(digit){
+	byCell = digit;
+	for(let i = 0; i < byCell; i++)
+	{
+		addRows();
+	}
+	let gridChild = '', cell = '';
+	for(let y = 0; y < grid.children.length; y++)
+	{
+		gridChild = grid.children[y];
+		for(let z = 0; z < byCell; z++)
+		{
+			units = Math.floor(500 / byCell);
+			units = String(units) + 'px';
+			cell = document.createElement('div');
+			cell.className = 'cell';
+			gridChild.style.setProperty('height', units);
+			cell.style.setProperty('height', units);
+			cell.style.setProperty('width', units);
+			gridChild.appendChild(cell);
 		}
-		return '#' + randColor;
-	};
-
-	var addRows = function(){
-		$('<div>').addClass('row').appendTo('#grid');
-			};
-
-	var switchState = Number(0);
-
-
-	function createCells(numOfCells){
-		var functionIterator = 0;
-		while (functionIterator != numOfCells){
-			$('<div>').addClass('cell').addClass('coloring').appendTo('.row:last-child');
-			functionIterator += 1;
-		}
 	}
+}
 
-	function createRows(gridSize){
-		var iterator = 0;
-		while (iterator != gridSize){
-				addRows();
-				createCells(gridSize);
-				iterator += 1;
-		}
+const emptyGrid = function(){
+	while(grid.children.length)
+		grid.children[0].remove();
+}
+
+let cells = document.querySelectorAll('.cell');
+
+const fillCells = function(){
+	cells = document.querySelectorAll('.cell');
+}
+
+let button = document.querySelector('#button');
+button.addEventListener('click', function(e){
+	e.preventDefault();
+	let count = inputField.value
+	emptyGrid();
+	makeTheGrid(count);
+	fillCells();
+	colorCells();
+})
+
+const paintColor = function(){
+	let colorSelect = document.querySelector(".controls form").children;
+	for(let col=0; col < colorSelect.length; col++)
+	{
+		colorSelect[col].addEventListener("click", function(){
+			painter = colorSelect[col].value;
+		})
 	}
+}
 
-	var randomChanger = setInterval(changeRandomColor, 1000);
+let painter = "white";
 
-	function changeRandomColor(){
-		$('#randomColor').css({"background-color": trulyRandomColorPicker});
-	}
+paintColor();
 
-	$(document).on("keypress", "form", function(event) { 
-    return event.keyCode != 13;
-	});
-
-	$('.control_buttons').on('click', function(event){
+let form = document.querySelector('center form'); 
+makeTheGrid(byCell);
+let inputField = document.querySelector('#inputField');
+const inputDragHandle = function(){
+	inputField.addEventListener('drag', function(event){
 		event.preventDefault();
-		$('#randomColor').on('click', function(){
-			switchState = 1;
-		});
-		$('#Black').on('click', function(){
-			switchState = 0;
-		});
-		$('#Erase').on('click', function(){
-			switchState = 2;
-		});
-	});
+	})
+}
 
-	createRows(16);
-	resizeDivs(16);
+inputDragHandle();
 
-	$("#button").on("click", function(event){
-		$('#grid').empty();
-		event.preventDefault();
-		$('#inputField').val(function(i, number){
-			gridSize = number;
-			//So far the code returns a numeric value for my gridSize variable on click of the button.
-			createRows(gridSize);
-			resizeDivs(gridSize);
-		});
-	});
+var randomChanger = setInterval(changeRandomColor, 1000);
 
+function changeRandomColor(){
+	document.querySelector("#randomColor").style.backgroundColor = trulyRandomColorPicker();
+}
 
-	$('#grid').on('click', '.cell', function(){
-		$('.cell').toggleClass('coloring');
-	});
+changeCellClass = function(cell){
+	let tokenList = cell.classList;
+	if(tokenList.contains("coloring"))
+		{
+			tokenList.remove("coloring");
+		}else {
+			tokenList.add("coloring");
+	}
+}
 
+function traverseCells(){
+	fillCells();
+	grid.addEventListener("click", function(){
+		cells.forEach(changeCellClass);
+	})
+}
 
-	$('#grid').on('click', '.cell', function(){
-		if (switchState == 0){
-			$('#grid').on('mouseenter', '.coloring', function(event){
-				event.preventDefault();
-				$(this).css({'background-color': 'black'});
-
-			});
-		}else if (switchState == 1) {
-			$('#grid').on('mouseenter', '.coloring', function(event){
-				event.preventDefault();
-				$(this).css({'background-color': trulyRandomColorPicker()});
-
-			});
-		}else if (switchState == 2){
-			$('#grid').on('mouseenter', '.coloring', function(event){
-				event.preventDefault();
-				$(this).css({'background-color':'white'});
-
-			});
+function coloring(cell){
+	let tokenList = cell.classList;
+	cell.addEventListener("mouseenter", function(){
+		console.log(cell);
+		if(tokenList.contains("coloring") && painter !== "random"){
+			cell.style.backgroundColor = painter;
+		}else if (tokenList.contains("coloring")){
+			cell.style.backgroundColor = trulyRandomColorPicker();
 		}
-	});
-});
+	})
+}
 
-//need to figure out a way to take the value from the input and create the amount of divs needed for it.
-//$('<div>').addClass('row').appendTo('#grid');
-//	$('#grid').on('mouseenter', '.cell', function(){
-//		$(this).css({'background-color':'black'});
-//	});
-
-//figure out how to inrtoduce a random color value to my JavaScript. 
+function colorCells(){
+	cells.forEach(coloring);
+}
+traverseCells();
+colorCells();
